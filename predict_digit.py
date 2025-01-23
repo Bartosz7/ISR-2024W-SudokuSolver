2f8e3d0d161b53a3e0134dac78e7533c20f27e6b
Weronika375 <plichta.weronika@gmail.com>
2025-01-23 13:21:20 +0100

    Adding softmax
    It helps with prediction blank spaces

--- predict_digit.py
+++ predict_digit.py
@@ -2,28 +2,30 @@ import torch
 from torchvision import transforms
 from PIL import Image
 
-
-def predict_digit(model, image_path):
-    transform = transforms.Compose(
-        [
-            transforms.Resize((64, 64)),
-            transforms.Grayscale(num_output_channels=1),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5], [0.5]),
-        ]
-    )
+def predict_digit(model, image_path, threshold=0.3):
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),           
+        transforms.Grayscale(num_output_channels=1),  
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])    
+    ])
 
     image = Image.open(image_path)
-    image = transform(image).unsqueeze(0)
+    image = transform(image).unsqueeze(0)  
 
     device = next(model.parameters()).device
     image = image.to(device)
 
+    # Inference
     with torch.no_grad():
-        outputs = model(image)
-        _, predicted = torch.max(outputs, 1)
+        outputs = model(image)            
+        max_probs, predicted = torch.max(outputs, 1)
+        max_prob = max_probs.item()
 
-    return predicted.item() + 1
+    if max_prob < threshold:
+        return None
+    else:
+        return predicted.item() + 1 
 
 
 # model = load_model('/content/digit1.pth')
--- scripts/cnn_arch.py
+++ scripts/cnn_arch.py
@@ -43,8 +43,10 @@ class SimpleDigitCNN(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
 
+
         self.pool = nn.MaxPool2d(kernel_size=2)
         self.dropout = nn.Dropout(dropout_p)
+        self.softmax = nn.Softmax()
 
         # For an input size of 64x64 (after transforms):
         # After conv1 -> pool -> conv2 -> pool -> conv3 -> pool
@@ -62,6 +64,7 @@ class SimpleDigitCNN(nn.Module):
         x = nn.ReLU()(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
+        x = self.softmax(x)
         return x
 
 
